5e3112989e70e2ec7c290daadac7b77192f695f4
cllyrdkl <celalettinyurdakul@gmail.com>
2018-11-12 15:51:56 -0500

    Defocus acqusition with Thorlabs KDC101
    Added Thorlabs KDC101 stage  with specified S/N of the controllers
    Added defocus acquisition option
    Added modular acquisition option

--- spincam.py
+++ spincam.py
@@ -8,12 +8,10 @@ import numpy as np
 
 __SYSTEM = PySpin.System.GetInstance()
 
-
 __CAM = None
 
 
 def __destructor():
-
     print('Cleaning up SpinCam...')
 
     # Cleanup
@@ -21,8 +19,10 @@ def __destructor():
 
     if __SYSTEM.IsInUse():
         print('System is still in use')
-    
+
+
 atexit.register(__destructor)
+
 
 def __cam_node_cmd(cam, cam_attr_str, cam_method_str, pyspin_mode_str=None, cam_method_arg=None):
     # Performs cam_method on input cam with optional access mode check
@@ -55,10 +55,11 @@ def __cam_node_cmd(cam, cam_attr_str, cam_method_str, pyspin_mode_str=None, cam_
                                    'supported...')
 
     # Perform command
-    if cam_method_arg is None: #pylint: disable=no-else-return
+    if cam_method_arg is None:  # pylint: disable=no-else-return
         return getattr(cam_attr, cam_method_str)()
     else:
         return getattr(cam_attr, cam_method_str)(cam_method_arg)
+
 
 def __cleanup_cam():
     # cleans up camera
@@ -73,30 +74,32 @@ def __cleanup_cam():
     # Clear camera reference
     __CAM = None
 
+
 def __find_cam(cam_serial):
-    #returns camera object 
+    # returns camera object
 
     # Retrieve cameras from the system
     cam_list = __SYSTEM.GetCameras()
 
     # Find camera matching serial
     cam_found = None
-	
+
     for i, cam in enumerate(cam_list):
-	    cam_found = cam
+        cam_found = cam
     # Check to see if match was found
     if cam_found is None:
         print('Could not find camera with serial: "' + str(cam_serial) + '".')
         return False
-		
+
     return cam_found
-	
+
+
 def __get_image(cam):
     # Gets image and info from camera
 
     # Get image object
     image = cam.GetNextImage()
-	
+
     # Initialize image dict
     image_dict = {}
 
@@ -108,48 +111,52 @@ def __get_image(cam):
         image_dict['bitsperpixel'] = image.GetBitsPerPixel()
     image.Release()
     return image_dict
-	
-def __get_image_and_avg(cam,num_to_avg):
-	# Gets images and info from camera
-	try:
-		for i in range(0,num_to_avg):
-			# Get image object
-			image = cam.GetNextImage()
 
-			# Initialize image dict
-			image_dict = {}
 
-			# Ensure image is complete
-			if not image.IsIncomplete():
-			# Get data/metadata
-				if i == 0:
-					img_array=np.array(image.GetNDArray(),dtype=np.float)
-					arr = img_array/num_to_avg
-					print('Frame: '+str(datetime.now()))
-				else:
-					img_array=np.array(image.GetNDArray(),dtype=np.float)+ img_array
-					arr=arr+img_array/num_to_avg
-					print('Frame: '+str(datetime.now()))
-			image.Release()
-		image_dict['data'] = arr
-		image_dict['timestamp'] = image.GetTimeStamp()
-		print('Averaged Frame: '+str(datetime.now()))
-		image_dict['bitsperpixel'] = image.GetBitsPerPixel()
-	except PySpin.SpinnakerException as ex:
-		print('Error: %s' % ex)
-		return False
+def __get_image_and_avg(cam, num_to_avg):
+    # Gets images and info from camera
+    try:
+        for i in range(0, num_to_avg):
+            # Get image object
+            image = cam.GetNextImage()
 
-	return image_dict
-	
+            # Initialize image dict
+            image_dict = {}
+
+            # Ensure image is complete
+            if not image.IsIncomplete():
+                # Get data/metadata
+                if i == 0:
+                    img_array = np.array(image.GetNDArray(), dtype=np.float)
+                    arr = img_array / num_to_avg
+                    print('Frame: ' + str(datetime.now()))
+                else:
+                    img_array = np.array(image.GetNDArray(), dtype=np.float) + img_array
+                    arr = arr + img_array / num_to_avg
+                    print('Frame: ' + str(datetime.now()))
+            image.Release()
+        image_dict['data'] = arr
+        image_dict['timestamp'] = image.GetTimeStamp()
+        print('Averaged Frame: ' + str(datetime.now()))
+        image_dict['bitsperpixel'] = image.GetBitsPerPixel()
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        return False
+
+    return image_dict
+
+
 def __init_cam(cam):
-	# Init() camera
-	cam.Init()
-			 
+    # Init() camera
+    cam.Init()
+
+
 def __validate_cam(cam, cam_str):
     # Checks to see if camera is valid
 
     if not cam.IsValid():
         raise RuntimeError('Camera is not valid.')
+
 
 def __validate_cam_init(cam, cam_str):
     # Checks to see if camera is valid and initialized
@@ -159,6 +166,7 @@ def __validate_cam_init(cam, cam_str):
     if not cam.IsInitialized():
         raise RuntimeError('Camera is not initialized.')
 
+
 def __validate_cam_streaming(cam, cam_str):
     # Checks to see if camera is valid, initialized, and streaming
 
@@ -166,9 +174,11 @@ def __validate_cam_streaming(cam, cam_str):
 
     if not cam.IsStreaming():
         raise RuntimeError(cam_str + ' cam is not streaming. Please start_acquisition() it.')
-	
+
+
 def __roi():
-	return 0
+    return 0
+
 
 def set_video_mode(mode):
     global __CAM
@@ -178,7 +188,7 @@ def set_video_mode(mode):
     if not PySpin.IsAvailable(node_video_mode) or not PySpin.IsWritable(node_video_mode):
         print('Unable to set VideoMode. Aborting...')
         return False
-		
+
     node_video_mode_7 = node_video_mode.GetEntryByName("Mode7")
     if not PySpin.IsAvailable(node_video_mode_7) or not PySpin.IsReadable(node_video_mode_7):
         print('Unable to set VideoMode to Mode7 (entry retrieval). Aborting...')
@@ -186,7 +196,8 @@ def set_video_mode(mode):
 
     video_mode_7 = node_video_mode_7.GetValue()
     node_video_mode.SetIntValue(video_mode_7)
-    print('Video Mode set to Mode '+ mode)
+    print('Video Mode set to Mode ' + mode)
+
 
 def __get_and_validate_init_cam():
     # Validates initialization and returns it
@@ -195,13 +206,15 @@ def __get_and_validate_init_cam():
     __validate_cam_init(cam, 'Camera')
     return cam
 
+
 def __get_and_validate_streaming_cam():
     # Validates streaming of camera then returns it
 
     cam = __get_cam()
     __validate_cam_streaming(cam, 'Camera')
-    return cam	
-	
+    return cam
+
+
 def __get_cam():
     # Returns Camera
 
@@ -209,33 +222,36 @@ def __get_cam():
         raise RuntimeError('Camera not found')
 
     return __CAM
-	
-	### Public Functions ###
+
+
+### Public Functions ###
 
 def cam_node_cmd(cam_attr_str, cam_method_str, pyspin_mode_str=None, cam_method_arg=None):
-
     return __cam_node_cmd(__get_and_validate_init_cam(),
                           cam_attr_str,
                           cam_method_str,
                           pyspin_mode_str,
                           cam_method_arg)
 
-	
+
 def get_image():
     # Gets image from camera 
     return __get_image(__get_and_validate_streaming_cam())
 
+
 def get_image_and_avg(num_to_avg):
     # Gets and averages images from camera 
-	return __get_image_and_avg(__get_and_validate_streaming_cam(),num_to_avg)
-	
+    return __get_image_and_avg(__get_and_validate_streaming_cam(), num_to_avg)
+
+
 def end_acquisition():
     # Ends acquisition
     __get_and_validate_streaming_cam().EndAcquisition()
 
+
 def find_cam(cam_serial):
     # Finds Camera
-    global __CAM 
+    global __CAM
 
     cam = __find_cam(cam_serial)
 
@@ -245,40 +261,46 @@ def find_cam(cam_serial):
     # Assign camera
     __CAM = cam
 
-    print('Found camera')	
-	
+    print('Found camera')
+
+
 def set_gain(gain):
-	cam_node_cmd('Gain',
-                     'SetValue',
-                     'RW',
-                     gain)
+    cam_node_cmd('Gain',
+                 'SetValue',
+                 'RW',
+                 gain)
+
 
 def set_exposure(exposure):
-	cam_node_cmd('ExposureTime',
-                     'SetValue',
-                     'RW',
-                     exposure)
-					 
+    cam_node_cmd('ExposureTime',
+                 'SetValue',
+                 'RW',
+                 exposure)
+
+
 def set_frame_rate(frame_rate):
     cam_node_cmd('AcquisitionFrameRate',
-                     'SetValue',
-                     'RW',
-                     frame_rate)
+                 'SetValue',
+                 'RW',
+                 frame_rate)
+
 
 def disable_auto_exp():
-	print('Disabling Auto Exposure')	
-	cam_node_cmd('ExposureAuto',
-                     'SetValue',
-                     'RW',
-                     PySpin.ExposureAuto_Off)
-					 
+    print('Disabling Auto Exposure')
+    cam_node_cmd('ExposureAuto',
+                 'SetValue',
+                 'RW',
+                 PySpin.ExposureAuto_Off)
+
+
 def disable_auto_gain():
-	print('Disabling Auto Gain')
-	cam_node_cmd('GainAuto',
-                     'SetValue',
-                     'RW',
-                     PySpin.GainAuto_Off)
-					 
+    print('Disabling Auto Gain')
+    cam_node_cmd('GainAuto',
+                 'SetValue',
+                 'RW',
+                 PySpin.GainAuto_Off)
+
+
 def disable_auto_frame():
     global __CAM
     nodemap = __CAM.GetNodeMap()
@@ -289,54 +311,59 @@ def disable_auto_frame():
         return False
     node_acquisition_en.SetValue(True)
     print('Enabling frame rate control')
-	
+
     node_acquisition_auto = PySpin.CEnumerationPtr(nodemap.GetNode('AcquisitionFrameRateAuto'))
-    if not PySpin.IsAvailable(node_acquisition_auto) or not PySpin.IsWritable(node_acquisition_auto):
+    if not PySpin.IsAvailable(node_acquisition_auto) or not PySpin.IsWritable(
+            node_acquisition_auto):
         print('Unable to turn off Auto Frame Rate. Aborting...')
         return False
     node_acquisition_auto_off = node_acquisition_auto.GetEntryByName('Off')
-    if not PySpin.IsAvailable(node_acquisition_auto_off) or not PySpin.IsReadable(node_acquisition_auto_off):
+    if not PySpin.IsAvailable(node_acquisition_auto_off) or not PySpin.IsReadable(
+            node_acquisition_auto_off):
         print('Unable to turn off Auto Frame Rate. Aborting...')
         return False
     node_acquisition_auto.SetIntValue(node_acquisition_auto_off.GetValue())
 
+
 def set_gamma(gamma_val):
-	print('Setting Gamma to '+ str(gamma_val))
-	cam_node_cmd('Gamma',
-                     'SetValue',
-                     'RW',
-                     gamma_val)
-					 
+    print('Setting Gamma to ' + str(gamma_val))
+    cam_node_cmd('Gamma',
+                 'SetValue',
+                 'RW',
+                 gamma_val)
+
+
 def get_exp_min():
-	 
-	return cam_node_cmd('ExposureTime','GetMin')
-	
+    return cam_node_cmd('ExposureTime', 'GetMin')
+
+
 def get_exp_max():
-	 
-	return cam_node_cmd('ExposureTime','GetMax')
+    return cam_node_cmd('ExposureTime', 'GetMax')
+
 
 def get_fps_min():
-	 
-	return cam_node_cmd('AcquisitionFrameRate','GetMin')
-	
+    return cam_node_cmd('AcquisitionFrameRate', 'GetMin')
+
+
 def get_fps_max():
-	 
-	return cam_node_cmd('AcquisitionFrameRate','GetMax')
-	
+    return cam_node_cmd('AcquisitionFrameRate', 'GetMax')
+
+
 def get_frame_rate():
-
     return cam_node_cmd('AcquisitionFrameRate',
-                     'GetValue')		
-	
-def init_cam():
-	# Initializes camera		 
-	__init_cam(__get_cam())
+                        'GetValue')
 
-	
+
+def init_cam():
+    # Initializes camera
+    __init_cam(__get_cam())
+
+
 def start_acquisition():
     # Starts acquisition
     __get_and_validate_init_cam().BeginAcquisition()
-	
+
+
 def roi():
     # Select Region of Interest
     return __roi()
---
+++ stage.py
@@ -0,0 +1,99 @@
+import thorlabs_apt as apt
+import time
+__cntrl_x = None
+__cntrl_y = None
+__cntrl_z = None
+
+def __initialize_stages():
+    global __cntrl_x
+    global __cntrl_y
+    global __cntrl_z
+
+    # get current device lists
+    devices = apt.list_available_devices()
+
+    # SNs of the KDC101 devices
+    x_stage = 27002265
+    y_stage = 27002165
+    z_stage = 27002158
+
+    # initialize the motor controllers
+    __cntrl_x = apt.Motor(x_stage)
+    __cntrl_y = apt.Motor(y_stage)
+    __cntrl_z = apt.Motor(z_stage)
+    print('Stages are initialized...')
+
+    # default positions of x,y, and z all in microns
+    x_pos = 5
+    y_pos = 5
+    z_pos = 5
+    
+    # homing
+    __cntrl_x.move_home()
+    time.sleep(1)    # pause 5 to homing
+    __cntrl_y.move_home()
+    time.sleep(1)    # pause 5 to homing
+    __cntrl_z.move_home()
+    time.sleep(60)    # pause 5 to homing
+    print('Homing is finished...')
+	
+    # go to home positions
+    __cntrl_x.move_to(x_pos)
+    time.sleep(1)
+    __cntrl_y.move_to(y_pos)
+    time.sleep(1)
+    __cntrl_z.move_to(z_pos)
+    time.sleep(20)    # pause 5 to homing
+    print('Stages are ready...')
+
+
+
+def __go_y(position):
+    global __cntrl_y
+    # go to positions
+    __cntrl_y.move_by(position*1e-3)
+
+
+def __go_x(position):
+    global __cntrl_x
+    # go to positions
+    __cntrl_x.move_by(position*1e-3)
+
+
+def __go_z(position):
+    global __cntrl_z
+    # go to positions
+    __cntrl_z.move_by(position*1e-3)
+
+
+""" 
+import thorlabs_apt as apt
+
+__cntrl_x = None
+__cntrl_y = None
+__cntrl_z = None
+
+
+def __initialize_stages():
+    global __cntrl_x
+    global __cntrl_y
+    global __cntrl_z
+
+
+
+
+def __go_y(position):
+    print(position)
+
+    # go to positions
+
+
+def __go_x(position):
+    print(position)
+    # go to positions
+
+
+def __go_z(position):
+    print(position)
+    # go to positions
+"""
